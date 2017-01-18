1c7e67aad27b18236e0e7189f2a846629f356ecd
LiuShuang <2216336012@qq.com>
2017-01-18 09:40:08 +0800

    modified Project

---
+++ V2EX_C/V2EX_C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  V2EX_C
+//
+//  Created by LiuShuang on 2017/1/18.
+//  Copyright © 2017年 LiuShuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ V2EX_C/V2EX_C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  V2EX_C
+//
+//  Created by LiuShuang on 2017/1/18.
+//  Copyright © 2017年 LiuShuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
